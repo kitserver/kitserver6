86a898aac1015663681871d3760abbd8789dd231
juce <juce66@gmail.com>
2008-05-29 15:53:05 +0000

    sample configuration for building with VC++ 6.0

---
+++ src/setenv.vc60.cmd
@@ -0,0 +1,9 @@
+@echo off
+echo Setting kitserver compile environment
+@call "c:\vc60\VC98\bin\vcvars32.bat"
+set DXSDK=c:\dxsdk81
+set STLPORT=c:\stlport-4.6.2
+set INCLUDE=%STLPORT%\stlport;%DXSDK%\include;%INCLUDE%
+set LIB=%STLPORT%\lib;%DXSDK%\lib;%LIB%
+echo Environment set
+
