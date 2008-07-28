7f6805f9b810147c902eab85a9720bd2ed5d6eea
juce <juce66@gmail.com>
2008-07-28 22:09:52 +0000

    adding name-mask feature

--- src/afsexport.cpp
+++ src/afsexport.cpp
@@ -6,7 +6,7 @@
 
 KSERV_CONFIG g_config;
 
-void ExportFile(char* afsFileName, int id)
+void ExportFile(char* afsFileName, int id, const char* nameMask)
 {
 	AFSITEMINFO itemInfo;
 	ZeroMemory(&itemInfo, sizeof(AFSITEMINFO));
@@ -23,7 +23,7 @@ void ExportFile(char* afsFileName, int id)
 	fseek(f, itemInfo.dwOffset, SEEK_SET);
 	char szFileName[32];
 	ZeroMemory(szFileName, sizeof(szFileName));
-	sprintf(szFileName, "id_%00005d.bin", id);
+	sprintf(szFileName, nameMask, id);
 	FILE* out = fopen(szFileName, "wb");
 	BYTE* buf = (BYTE*)HeapAlloc(GetProcessHeap(), HEAP_ZERO_MEMORY, itemInfo.dwSize);
 	if (fread(buf, itemInfo.dwSize, 1, f) == 1) {
@@ -43,25 +43,29 @@ void ExportFile(char* afsFileName, int id)
 void main(int argc, char* argv[])
 {
 	char afsFileName[1024];
-	ZeroMemory(afsFileName, 1024);
+    char nameMask[128];
+	ZeroMemory(afsFileName, sizeof(afsFileName));
+	ZeroMemory(nameMask, sizeof(nameMask));
 
-	if (argc < 2)
+	if (argc < 3)
 	{
-		printf("Usage: %s <file-id(s)> [<afs-file>]\n", argv[0]);
+		printf("Usage: %s <file-id(s)> <afs-file> [<name-mask>]\n", argv[0]);
 		return;
 	}
 
-	// if specific filename not specified, assume default AFS
-	if (argc > 2)
-		lstrcpy(afsFileName, argv[2]);
+    lstrcpy(afsFileName, argv[2]);
+
+	// if specific nameMask not specified, assume default mask
+	if (argc > 3)
+		lstrcpy(nameMask, argv[3]);
 	else
-		lstrcpy(afsFileName, "0_text.afs");
+		lstrcpy(nameMask, "id_%00005d.bin");
 
     char* splitter = strchr(argv[1], '-');
     if (!splitter) {
         // single file
         int id = atoi(argv[1]);
-        ExportFile(afsFileName, id);
+        ExportFile(afsFileName, id, nameMask);
 
     } else {
         // range of files
@@ -69,7 +73,7 @@ void main(int argc, char* argv[])
         splitter[0] = '\0';
         int first = atoi(argv[1]);
         for (int id=first; id<=last; id++) {
-            ExportFile(afsFileName, id);
+            ExportFile(afsFileName, id, nameMask);
         }
     }
 }
