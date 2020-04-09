9286af8a3c9a3fce09d76ba9ffb5c0da36974769
Anton Jouline <juce66@gmail.com>
2020-04-09 10:15:15 -0700

    removing dxsdk81 vars: it is included in soft dir now

--- src/setenv.vs2012.cmd
+++ src/setenv.vs2012.cmd
@@ -1,8 +1,4 @@
 @echo off
 echo Setting kitserver compile environment
 @call "c:\vs11\VC\bin\vcvars32.bat"
-set DXSDK=c:\dxsdk81
-set INCLUDE=%INCLUDE%;%DXSDK%\include
-set LIB=%LIB%;%DXSDK%\lib
 echo Environment set
-
--- src/setenv.vs2019.cmd
+++ src/setenv.vs2019.cmd
@@ -1,8 +1,4 @@
 @echo off
 echo Setting kitserver compile environment
 @call "c:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvars32.bat"
-set DXSDK=c:\dxsdk81
-set INCLUDE=%INCLUDE%;%DXSDK%\include
-set LIB=%LIB%;%DXSDK%\lib
 echo Environment set
-
